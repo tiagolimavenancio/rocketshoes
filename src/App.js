b5e668e0f329f126cd1b6d93824af690028677b8
Tiago Lima <tiago.lima@osf.digital>
2020-06-06 12:07:39 -0300

    Navigated with Saga.

--- src/App.js
+++ src/App.js
@@ -7,11 +7,12 @@ import Routes from './routes';
 import GlobalStyle from './styles/globals';
 import Header from './components/Header';
 import store from './store';
+import history from './services/history';
 
 function App() {
   return (
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter history={history}>
         <Header />
         <GlobalStyle />
         <ToastContainer autoClose={3000} />
