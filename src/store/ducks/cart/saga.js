c3f0ddca6520205775a38e76541388f8714b4979
Tiago Lima <tiago.lima@osf.digital>
2020-06-06 11:39:50 -0300

    Added React Toastify.

--- src/store/ducks/cart/saga.js
+++ src/store/ducks/cart/saga.js
@@ -1,4 +1,5 @@
 import { call, select, put, all, takeLatest } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
 import api from '../../../services/api';
 import { addToCartSuccess, updateAmount } from './actions';
 import { formatPrice } from '../../../util/format';
@@ -15,7 +16,7 @@ function* addToCart({ id }) {
   const amount = currentAmount + 1;
 
   if (amount > stockAmount) {
-    console.tron.warn('ERROR!');
+    toast.error('Ordered quantity out of stock.');
     return;
   }
 
