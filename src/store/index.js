7c4b2942d40e6bc8ee9d27252a359c1da30ba476
Tiago Lima <tiago.lima@osf.digital>
2020-06-06 11:09:18 -0300

    Integrated Reactotron with Saga.

--- src/store/index.js
+++ src/store/index.js
@@ -3,7 +3,12 @@ import createSagaMiddleware from 'redux-saga';
 import reducers from './ducks/';
 import sagas from './ducks/sagas';
 
-const sagaMiddleware = createSagaMiddleware();
+const sagaMonitor =
+  process.env.NODE_ENV === 'development'
+    ? console.tron.createSagaMonitor()
+    : null;
+
+const sagaMiddleware = createSagaMiddleware({ sagaMonitor });
 
 const enhancer =
   process.env.NODE_ENV === 'development'
