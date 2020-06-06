8f059dd0b57681bf717311f1177102ba2d7c2a68
Tiago Lima <tiago.lima@osf.digital>
2020-06-06 10:01:59 -0300

    Displayed products amount.

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -7,7 +7,7 @@ import api from '../../services/api';
 import { formatPrice } from '../../util/format';
 import { ProductList } from './styles';
 
-function Home({ addToCart }) {
+function Home({ addToCart, amount }) {
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
@@ -38,7 +38,8 @@ function Home({ addToCart }) {
 
           <button type="button" onClick={() => onHandleAddProduct(product)}>
             <div>
-              <MdAddShoppingCart size={16} color="#fff" /> 3
+              <MdAddShoppingCart size={16} color="#fff" />{' '}
+              {amount[product.id] || 0}
             </div>
             <span>Add to Cart</span>
           </button>
@@ -48,7 +49,14 @@ function Home({ addToCart }) {
   );
 }
 
+const mapStateToProps = (state) => ({
+  amount: state.cart.reduce((amount, product) => {
+    amount[product.id] = product.amount;
+    return amount;
+  }, {}),
+});
+
 const mapDispatchToProps = (dispatch) =>
   bindActionCreators(CartActions, dispatch);
 
-export default connect(null, mapDispatchToProps)(Home);
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
